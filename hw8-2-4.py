f414c8c786e31f9f4a5d4265a1bf0eeb75ddefa2
p22cobrien <p22cobrien@fairifeldprep.org>
2021-12-08 17:09:10 -0500

    submit hw8-2-4.py

--- hw8-2-4.py
+++ hw8-2-4.py
@@ -1,1 +1,9 @@
 # Author: CMOB 12/7/2021
+
+def letter_count(word, letter):
+    total = str.count(word, letter)
+    return total
+
+print(letter_count("cat", "t") == 1)
+print(letter_count("apple", "p") == 2)
+print(letter_count("supercalifragilisticexpialidocious", "i") == 7)
