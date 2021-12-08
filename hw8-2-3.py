4b6507e337eed6364f73ba5354586027b4ba5c6d
p22cobrien <p22cobrien@fairifeldprep.org>
2021-12-07 22:01:41 -0500

    WIP

---
+++ hw8-2-3.py
@@ -0,0 +1,15 @@
+# Author: CMOB 12/7/2021
+
+def three_letter_words(word):
+    total = 0
+    for x in word:
+        if len(x) <= 3:
+            total += 1
+        else:
+            continue
+    x += 1
+    return total
+
+print(three_letter_words(["cat", "bat", "apple"]) == 2)
+print(three_letter_words(["apple", "hippo", "mouse"]) == 0)
+print(three_letter_words(["hop", "pop", "bop"]) == 3)
